eeb06b977143935b0511a2a5bbd68564ee02e6a9
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-02-21 12:51:34 +0200

    Finish registration process

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,26 @@
 class PlayersController < ApplicationController
 
   def new
+    @player = Player.new
+  end
 
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  def create
+    @player = Player.new(p_params)
+    if @player.save
+      flash[:success] = "Smooth sailing!"
+      redirect_to @player
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def p_params
+    params.require(:player).permit(:name, :email, :password, :password_confirmation)
   end
 end
