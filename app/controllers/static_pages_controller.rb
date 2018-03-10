6fc4e3fb620fd71ec687ed0f68376e8bf2e9ecf7
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-03-10 22:06:21 +0200

    add views newsposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
 
   def home
+    @newsposts = Newspost.all.paginate(page: params[:page], per_page: 5)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :newsposts
   resources :players
   resources :games, only: [:show, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
   root 'static_pages#home'
-  get 'games' => 'games#show'
+  #get 'games' => 'games#show'
   #match '/games', to: 'games#show', via: 'get'
   #get '/games/:id', to: 'games#show'
   match '/signup', to: 'players#new', via: 'get'
   match '/signin', to: 'sessions#new', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
+  match '/post', to: 'newsposts#new', via: 'get'
+
+  match '/move/:id/cell=:cell', to: 'games#receive_move', via: 'get'
 
  end
